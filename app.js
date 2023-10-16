a437f44e7743b81adcad9f3fdd6e99224fc155e8
21120447 <haonhat2729@gmail.com>
2023-10-16 20:52:34 +0700

    aa

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const bodyParser = require("body-parser");
 const db = mysql.createConnection({
   host: process.env.DATABASE_HOST,
   user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
   database: process.env.DATABASE,
 });
 
